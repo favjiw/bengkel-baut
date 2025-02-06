4a61fd308490c22d77a3440804826c9f2c6d4dad
JuliusBourbon <naherrrrr@gmail.com>
2025-02-06 16:53:58 +0700

    Update Landing Page

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -2,16 +2,38 @@
 <html>
 <head>
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <div class="topnav">
-    <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact Us</a></li>
-    </ul>
+    <div class="items">
+        <div class="logo"><img src="https://kpssteel.com/storage/2022/10/perbedaan-baut-dan-sekrup-KPS-Steel-distributor-besi-jakarta-1024x536.jpg" alt=""></div>
+        <div class="login">
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+        </div>
+    </div>
 </div>
 <body>
     <div id="bannerimage"></div>
-    <h1>tes</h1>
+    <h1 style="text-align: center">Kami menyediakan:</h1>
+    <div class="packets">
+        <div class="packet">
+            <img src="https://kpssteel.com/storage/2022/10/perbedaan-baut-dan-sekrup-KPS-Steel-distributor-besi-jakarta-1024x536.jpg" alt="">
+            Bongkar
+        </div>
+        <div class="packet">
+            <img src="https://kpssteel.com/storage/2022/10/perbedaan-baut-dan-sekrup-KPS-Steel-distributor-besi-jakarta-1024x536.jpg" alt="">
+            Pasang
+        </div>
+        <div class="packet">
+            <img src="https://kpssteel.com/storage/2022/10/perbedaan-baut-dan-sekrup-KPS-Steel-distributor-besi-jakarta-1024x536.jpg" alt="">
+            Bongkar dan Pasang
+        </div>
+    </div>
+    <div class="button">
+        <button>Pesan Sekarang</button>
+    </div>
 </body>
 </html>
