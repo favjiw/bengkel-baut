0f5986099da79c3b89e2a0a1c950fa0e32e7116b
AlghifariRaspati <jsonrsf@gmail.com>
2025-02-06 15:10:23 +0700

    Landing Page

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<div class="topnav">
+    <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact Us</a></li>
+    </ul>
+</div>
+<body>
+    <div id="bannerimage"></div>
+    <h1>tes</h1>
+</body>
+</html>
